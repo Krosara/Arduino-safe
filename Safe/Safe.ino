da6194e7cf0a7aece516393e38aa525f3805167e
Kristian Savov <kristian.savov@gmail.com>
2020-10-16 15:11:06 +0200

    added functionality

--- Safe/Safe.ino
+++ Safe/Safe.ino
@@ -17,19 +17,20 @@ const int LED_GREEN = 5;
 const int BUZZER = 3;
 const int MIN_LDR_VALUE = 0;
 const int MAX_LDR_VALUE = 1023;
-const int MIN_LDR_THRESHOLD = 150;
+const int MIN_LDR_THRESHOLD = 200;
 const int MAX_LDR_THRESHOLD = 900;
 
 String passCode = "4312";   //predefined password
 String inputCode = "1---";
 String finalInputCode = "";
 int inputFailed = 0;    //failed attempts at the code
+bool isLocked = true;
 
+//button debounce
 int currentInputState;
 int currentConfirmState;
 int lastInputState;
 int lastConfirmState;
-
 
 int valueDigit = 0;     //for entering the code 1-4
 int positionDigit = 1;  //position on the display
@@ -86,7 +87,7 @@ void loop()
   {
     if (inputFailed < 3)
     {
-      if (positionDigit <= 4)
+      if (positionDigit <= 4 && isLocked == true)
       {
         buttonInput();
         if (detectInput)
@@ -111,21 +112,19 @@ void loop()
         finalInputCode.remove(4, 3);
         if (finalInputCode == passCode)
         {
-          if (!alreadyExecuted)
+          isLocked = false;
+          if (alreadyExecuted == false)
           {
             digitalWrite(LED_GREEN, HIGH);
             delay(500);
             digitalWrite(LED_GREEN, LOW);
           }
-          else
+          else if (LDR < MIN_LDR_THRESHOLD && isLocked == false)
           {
             valueDigit = 1;
             positionDigit = 1;
             inputCode = "1---";
             finalInputCode = "";
-            digitalWrite(LED_GREEN, HIGH);
-            delay(500);
-            digitalWrite(LED_GREEN, LOW);
           }
           buttonInput();
         }
@@ -141,14 +140,6 @@ void loop()
           inputCode = "1---";
           finalInputCode = "";
         }
-        if (LDR >= MIN_LDR_THRESHOLD)
-        {
-          valueDigit = 1;
-          positionDigit = 1;
-          inputCode = "1---";
-          finalInputCode = "";
-          digitalWrite(LED_GREEN, LOW);
-        }
       }
     }
     else
@@ -161,6 +152,7 @@ void loop()
       positionDigit = 1;
       inputCode = "1---";
       finalInputCode = "";
+      inputFailed = 0;
     }
   }
   else
@@ -174,5 +166,6 @@ void loop()
     positionDigit = 1;
     inputCode = "1---";
     finalInputCode = "";
+    inputFailed = 0;
   }
 }
